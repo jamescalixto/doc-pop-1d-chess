c7796f44c0280c3ea2675492b828769a57cf7d2f
James Calixto <jc2386@cornell.edu>
2021-07-07 22:10:50 -0400

    update

--- src/evaluate.py
+++ src/evaluate.py
@@ -33,7 +33,7 @@ def score_position_estimate(position):
 
 @functools.lru_cache(maxsize=CACHE_SIZE)
 def score_position_definite(position):
-    """Given a position, score it for the given player if the game is over."""
+    """Given a position, score it if the game is over."""
     board, active, halfmove, fullmove = position.split(" ")
     state = Position.check_position(position)
     if int(fullmove) >= MAX_FULLMOVES:
@@ -49,59 +49,20 @@ def score_position_definite(position):
 
 
 @functools.lru_cache(maxsize=CACHE_SIZE)
-def score_position_dfs(position):
-    """Given a position, score the best possible score for the current player
-    (assuming that the opponent plays optimally) and return the path to that end state.
-    Uses depth-first-search recursively."""
+def score_position_bfs(
+    position, depth=0, max_depth=None, max_depth_estimator=score_position_estimate
+):
+    """Given a position, score it (assuming that the opponent plays optimally) and
+    return the path to that end state. Uses breadth-first-search recursively with a
+    depth limit, after which it estimates the position using an estimator function."""
     board, active, halfmove, fullmove = position.split(" ")
 
-    # Base case: return score if game is over.
-    definite_score = score_position_definite(position)
-    if definite_score is not None:
-        return definite_score, []
-
-    # Otherwise, the score is the inverse of the best score the opponent can make.
-    moves = Position.get_current_moves(position)
-    potential_responses = []
-    for move in moves:
-        # Make the move and score the new position for the other player.
-        # Remember that we want potential_score to be as HIGH as possible if white is
-        # the active player and LOW as possible if black is the active player.
-        new_position = Position.apply_move(position, move)
-        potential_score, potential_moves = score_position(new_position)
-
-        # If the opponent loses with this move, then this move wins this position.
-        # This is the best possible outcome so we don't have to search anymore.
-        # Return that we win and also return this move, plus the moves that lead to that
-        # end state.
-        if active == "w" and potential_score == SCORE_WHITE_WIN:
-            return SCORE_WHITE_WIN, [move] + potential_moves
-        if active == "b" and potential_score == SCORE_BLACK_WIN:
-            return SCORE_BLACK_WIN, [move] + potential_moves
-
-        # Otherwise, keep track of the potential responses.
-        else:
-            potential_responses.append((potential_score, potential_moves))
-
-    # Get the shortest move path.
-    potential_responses = sorted(
-        potential_responses,
-        key=lambda response: len(response[1]),
-        reverse=False,
-    )
-    # Now sort. We pick the first element in this array so we want the first element to
-    # be the lowest if black is the active player.
-    potential_responses = sorted(
-        potential_responses, key=lambda response: response[0], reverse=(active == "w")
-    )
-
-    best_response = potential_responses[0]
-
-    return best_response[0], ([move] + best_response[1])
+    if Position.check_position(position) != (None, None):  # game is over
+        pass
 
 
 def test_score_position(position):
-    score, moves = score_position_dfs(position)
+    score, moves = score_position_bfs(position)
     print("score={}".format(score))
     Position.playback_moves(position, moves)
 
