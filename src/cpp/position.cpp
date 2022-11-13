6be4d12d97ae5309e68223418e53b18e1ee0a290
James Calixto <jc2386@cornell.edu>
2022-11-13 18:20:09 -0500

    more changes

--- src/cpp/position.cpp
+++ src/cpp/position.cpp
@@ -786,7 +786,7 @@ void explore(unsigned int max_level)
 int main()
 {
     importLookupTables(attackLookup);
-    explore(5);
+    explore(12);
 
     // // debugPrint(getAttackedSquares(START_BOARD, false));
     // string fence = "KQRBNP....pnbrqk w 0 1";
